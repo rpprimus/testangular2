57ef67029d7426365d6b9b0459fe4ce6cc90a269
rpprimus <88758566+rpprimus@users.noreply.github.com>
2021-08-16 21:32:26 +0530

    code reverted back

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -62,10 +62,5 @@ describe('AppComponent', () => {
   fixture.detectChanges();
   expect(component.clickEvent).toBeTruthy();
 });
-  it('test case should failed ',()=>{
-  let event={target:{id:"menubtnnn"}};
-  component.clickEvent(event);
-  fixture.detectChanges();
-  expect(component.clickEvent).toBeFalsy();
-});
+
 });
