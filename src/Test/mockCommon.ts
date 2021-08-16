1277c9d3292cca7d62afd299836b7b4da8825bdd
rpprimus <88758566+rpprimus@users.noreply.github.com>
2021-08-16 14:51:31 +0530

    hi revert eventZip

--- src/Test/mockCommon.ts
+++ src/Test/mockCommon.ts
@@ -404,7 +404,7 @@ export class mockCommon {
           isActive: true,
           isRequired: true,
           fieldSequence: 0,
-          dbPropertyName: "eventZip1",
+          dbPropertyName: "eventZip",
           value: "test"
         },
         {
