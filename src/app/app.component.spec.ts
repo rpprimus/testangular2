5609cfef4b11f73a6bc0376e2c05026e775cefd0
rpprimus <88758566+rpprimus@users.noreply.github.com>
2021-08-17 16:36:02 +0530

    fail test case testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -63,4 +63,10 @@ describe('AppComponent', () => {
   expect(component.clickEvent).toBeTruthy();
 });
 
+  it('test case should failed ',()=>{
+  let event={target:{id:"menubtnnn"}};
+  component.clickEvent(event);
+  fixture.detectChanges();
+  expect(component.clickEvent).toBeFalsy();
+});
 });
